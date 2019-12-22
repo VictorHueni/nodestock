779e185ee446a03fe22929835dd2e69237e2e4cb
Victor Hueni <victor.hueni@protonmail.com>
2019-12-22 22:13:05 +0100

    Connect to IEX API, Create Call_API Function, Manage GEt & POST Routes, Format API Data rendering

--- app.js
+++ app.js
@@ -4,20 +4,50 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const exphbs  = require('express-handlebars');
+const request = require('request');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 5000;
 
-const API_KEY = 'pk_56ac14c6600b4f109f90d1c8704f9855';
+//use body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+
+const API_KEY = '';
+
+function call_api(finishedAPI, stock_ticker){
+    request('https://cloud.iexapis.com/stable/stock/'+ stock_ticker +'/quote?token=' + API_KEY, { json:true }, (err, res, body) => {
+        if(err) { return console.log(err); }
+        if (res.statusCode === 200){
+            finishedAPI(body);
+        }
+    });
+};
+
 
 //set handlebars middleware
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-//set home handlebars routes
+//set home handlebars routes GET
 app.get('/', function (req, res) {
-    res.render('home', {
-        stuff: "This is Stuff"
-    });
+
+    call_api(function(doneAPI){
+
+        res.render('home', {
+            stock: doneAPI
+        });
+    }, 'FB');
+
+});
+//set home handlebars routes POST
+app.post('/', function (req, res) {
+
+    call_api(function(doneAPI){
+        res.render('home', {
+            stock: doneAPI
+        });
+    }, req.body.stockTicker);
+
 });
 //set about handlebars routes
 app.get('/about.html', function (req, res) {
